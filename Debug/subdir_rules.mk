4783dc447fe876dcd22662ea9f7074b381885294
Stolk <USTOLLU@PEROOT.COM>
2015-11-17 13:19:19 -0600

    Upload project

---
+++ Debug/subdir_rules.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+cgifuncs.obj: ../cgifuncs.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv5/tools/compiler/tms470_4.9.5/bin/cl470" -mv7M3 --code_state=16 --abi=eabi -me -O2 -g --include_path="C:/ti/ccsv5/tools/compiler/tms470_4.9.5/include" --include_path="C:/Users/ustollu/workspace_v5_2/morse_testing" --include_path="C:/StellarisWare/boards/ek-lm3s8962" --include_path="C:/StellarisWare" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/src/include" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/src/include/ipv4" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/apps" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/ports/stellaris/include" --gcc --define=ccs="ccs" --define=PART_LM3S8962 --diag_warning=225 --display_error_number --gen_func_subsections=on --ual --preproc_with_compile --preproc_dependency="cgifuncs.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+enet_io.obj: ../enet_io.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv5/tools/compiler/tms470_4.9.5/bin/cl470" -mv7M3 --code_state=16 --abi=eabi -me -O2 -g --include_path="C:/ti/ccsv5/tools/compiler/tms470_4.9.5/include" --include_path="C:/Users/ustollu/workspace_v5_2/morse_testing" --include_path="C:/StellarisWare/boards/ek-lm3s8962" --include_path="C:/StellarisWare" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/src/include" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/src/include/ipv4" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/apps" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/ports/stellaris/include" --gcc --define=ccs="ccs" --define=PART_LM3S8962 --diag_warning=225 --display_error_number --gen_func_subsections=on --ual --preproc_with_compile --preproc_dependency="enet_io.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+io.obj: ../io.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv5/tools/compiler/tms470_4.9.5/bin/cl470" -mv7M3 --code_state=16 --abi=eabi -me -O2 -g --include_path="C:/ti/ccsv5/tools/compiler/tms470_4.9.5/include" --include_path="C:/Users/ustollu/workspace_v5_2/morse_testing" --include_path="C:/StellarisWare/boards/ek-lm3s8962" --include_path="C:/StellarisWare" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/src/include" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/src/include/ipv4" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/apps" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/ports/stellaris/include" --gcc --define=ccs="ccs" --define=PART_LM3S8962 --diag_warning=225 --display_error_number --gen_func_subsections=on --ual --preproc_with_compile --preproc_dependency="io.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+lmi_fs.obj: ../lmi_fs.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv5/tools/compiler/tms470_4.9.5/bin/cl470" -mv7M3 --code_state=16 --abi=eabi -me -O2 -g --include_path="C:/ti/ccsv5/tools/compiler/tms470_4.9.5/include" --include_path="C:/Users/ustollu/workspace_v5_2/morse_testing" --include_path="C:/StellarisWare/boards/ek-lm3s8962" --include_path="C:/StellarisWare" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/src/include" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/src/include/ipv4" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/apps" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/ports/stellaris/include" --gcc --define=ccs="ccs" --define=PART_LM3S8962 --diag_warning=225 --display_error_number --gen_func_subsections=on --ual --preproc_with_compile --preproc_dependency="lmi_fs.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+startup_ccs.obj: ../startup_ccs.c $(GEN_OPTS) $(GEN_SRCS)
+	@echo 'Building file: $<'
+	@echo 'Invoking: ARM Compiler'
+	"C:/ti/ccsv5/tools/compiler/tms470_4.9.5/bin/cl470" -mv7M3 --code_state=16 --abi=eabi -me -O2 -g --include_path="C:/ti/ccsv5/tools/compiler/tms470_4.9.5/include" --include_path="C:/Users/ustollu/workspace_v5_2/morse_testing" --include_path="C:/StellarisWare/boards/ek-lm3s8962" --include_path="C:/StellarisWare" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/src/include" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/src/include/ipv4" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/apps" --include_path="C:/StellarisWare/third_party/lwip-1.3.2/ports/stellaris/include" --gcc --define=ccs="ccs" --define=PART_LM3S8962 --diag_warning=225 --display_error_number --gen_func_subsections=on --ual --preproc_with_compile --preproc_dependency="startup_ccs.pp" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Debug/third_party/lwip-1.3.2/apps/httpserver_raw/httpd.pp
@@ -0,0 +1,55 @@
+# FIXED
+
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/apps/httpserver_raw/httpd.c
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/debug.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/arch.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/ports/stellaris/include/arch/cc.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/stats.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/opt.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/Users/ustollu/workspace_v5_2/morse_testing/lwipopts.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/mem.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/memp.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/memp_std.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/apps/httpserver_raw/httpd.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/tcp.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/sys.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/pbuf.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/err.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/ipv4/lwip/ip.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/def.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/ipv4/lwip/ip_addr.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/ipv4/lwip/inet.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/netif.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/src/include/ipv4/lwip/icmp.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/third_party/lwip-1.3.2/apps/httpserver_raw/fs.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/ti/ccsv5/tools/compiler/tms470_4.9.5/include/string.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/ti/ccsv5/tools/compiler/tms470_4.9.5/include/linkage.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/StellarisWare/utils/ustdlib.h
+third_party/lwip-1.3.2/apps/httpserver_raw/httpd.obj: C:/ti/ccsv5/tools/compiler/tms470_4.9.5/include/stdarg.h
+
+C:/StellarisWare/third_party/lwip-1.3.2/apps/httpserver_raw/httpd.c: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/debug.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/arch.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/ports/stellaris/include/arch/cc.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/stats.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/opt.h: 
+C:/Users/ustollu/workspace_v5_2/morse_testing/lwipopts.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/mem.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/memp.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/memp_std.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/apps/httpserver_raw/httpd.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/tcp.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/sys.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/pbuf.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/err.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/ipv4/lwip/ip.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/def.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/ipv4/lwip/ip_addr.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/ipv4/lwip/inet.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/lwip/netif.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/src/include/ipv4/lwip/icmp.h: 
+C:/StellarisWare/third_party/lwip-1.3.2/apps/httpserver_raw/fs.h: 
+C:/ti/ccsv5/tools/compiler/tms470_4.9.5/include/string.h: 
+C:/ti/ccsv5/tools/compiler/tms470_4.9.5/include/linkage.h: 
+C:/StellarisWare/utils/ustdlib.h: 
+C:/ti/ccsv5/tools/compiler/tms470_4.9.5/include/stdarg.h: 
